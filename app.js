56ebf14499ff70f91b0330a2b963fbc296c8ee9c
luis <luic.sistem@gmail.com>
2020-02-07 12:51:07 -0500

    show more comics in dom

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 import Comic from "./scripts/comic.js"
 
-
+//instanciando objects
 const comic1 = new Comic("Capitan America", "https://comicvine1.cbsistatic.com/uploads/scale_large/6/67663/7234741-01.jpg", "Rahzzah")
-console.log(comic1);
+
 const comic2 = new Comic("Incredibles", "https://comicvine1.cbsistatic.com/uploads/scale_large/6/67663/7234170-01a.jpg","Jean-Claudio")
+const comic3 = new Comic("Gijoe", "https://comicvine1.cbsistatic.com/uploads/scale_large/6/67663/7233898-270a.jpg","Robert Atkins")
+const comic4 = new Comic("Young Justice", "https://comicvine1.cbsistatic.com/uploads/scale_large/6/67663/7233232-13.jpg","John Timms")
+const comic5 = new Comic("Batman", "https://comicvine1.cbsistatic.com/uploads/scale_large/6/67663/7232873-03.jpg","Robert Atkins")
+const comic6 = new Comic("Hulk ", "https://comicvine1.cbsistatic.com/uploads/scale_large/6/67663/7234744-01.jpg","Max Fiumara")
+
+
 
 const elem = document.getElementById("comics")
 
-function showComics (){
+// muestra comics en el dom
+function showComics (comic){
     const son = document.createElement("div")
+    son.classList.add("m-3")
 son.innerHTML = `
 <div class="card" style="width: 18rem;">
-  <img src="${comic1.getPoster()}" class="card-img-top" alt="">
+  <img src="${comic.getPoster()}" class="card-img-top" alt="">
   <div class="card-body">
-    <h5 class="card-title">${comic1.getName()}</h5>
-    <p class="card-text"> <strong> Creator</strong>: ${comic1.getCreator()} </p>
+    <h5 class="card-title ">${comic.getName()}</h5>
+    <p class="card-text"> <strong> Creator</strong>: ${comic.getCreator()} </p>
    
   </div>
 </div>
@@ -22,4 +30,9 @@ son.innerHTML = `
 elem.appendChild(son)
 
 }
-showComics()
+showComics(comic1)
+showComics(comic2)
+showComics(comic3)
+showComics(comic4)
+showComics(comic5)
+showComics(comic6)
